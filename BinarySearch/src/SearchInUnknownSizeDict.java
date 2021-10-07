2e2f18d4ee3b77ad7a189b8d9e20953518b10443
shawn-yin128 <yxyoung128@gmail.com>
2021-10-07 03:52:01 -0400

    binary search: 1. search in unknown size dict

---
+++ BinarySearch/src/SearchInUnknownSizeDict.java
@@ -0,0 +1,33 @@
+/**
+ * input unknown size dict, target
+ * find target or return -1
+ */
+public class SearchInUnknownSizeDict {
+    public static int searchInUnknownSizeDict(Dictionary dict, int target) {
+        if (dict == null) {
+            return -1;
+        }
+        int left = 0;
+        int right = 1;
+        while (dict.get(right) != null && dict.get(right) < target) {
+            left = right;
+            right = right * 2;
+        }
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (dict.get(mid) > target || dict.get(mid) == null) {
+                right = mid - 1;
+            } else if (dict.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
+/**
+ * time complexity: O(logn)
+ * space complexity: O(1)
+ */
