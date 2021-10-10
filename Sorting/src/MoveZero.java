c7e503dce5046eec1de7c7ff7c54d250e18d6493
shawn-yin128 <yxyoung128@gmail.com>
2021-10-10 12:45:29 -0400

    update

--- Sorting/src/MoveZero.java
+++ Sorting/src/MoveZero.java
@@ -1,2 +1,14 @@
+/**
+ * input an array, move all zero to right side, keep the non zero elements' relative position
+ */
 public class MoveZero {
+    public static void main(String[] args) {
+
+    }
+    public static void moveZero(int[] array) {
+
+    }
+    public static void swap(int[] array, int left, int right) {
+
+    }
 }
