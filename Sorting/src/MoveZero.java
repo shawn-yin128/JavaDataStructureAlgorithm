b3b5087d0427b5f4c63a8d9990948733356da35a
shawn-yin128 <yxyoung128@gmail.com>
2021-10-10 13:29:26 -0400

    update

--- Sorting/src/MoveZero.java
+++ Sorting/src/MoveZero.java
@@ -1,14 +1,33 @@
 /**
- * input an array, move all zero to right side, keep the non zero elements' relative position
+ * input an array, move all zero to right side
  */
 public class MoveZero {
     public static void main(String[] args) {
-
-    }
-    public static void moveZero(int[] array) {
-
+        int[] array = new int[] {0, 9, 8, 1, 0, 0, 2, 4, 0, 6};
+        moveZero(array);
+        for (int i =  0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
     }
     public static void swap(int[] array, int left, int right) {
-
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    public static void moveZero(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        int leftBound = 0;
+        int rightBound = array.length - 1;
+        while (leftBound <= rightBound) {
+            if (array[leftBound] != 0) {
+                leftBound++;
+            } else if (array[rightBound] == 0) {
+                rightBound--;
+            } else {
+                swap(array, leftBound++, rightBound--);
+            }
+        }
     }
 }
