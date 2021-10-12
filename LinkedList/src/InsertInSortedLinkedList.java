826ef9436491a0b0fecfc6957d7b1e7487db28cd
shawn-yin128 <yxyoung128@gmail.com>
2021-10-11 22:56:03 -0400

    update

--- LinkedList/src/InsertInSortedLinkedList.java
+++ LinkedList/src/InsertInSortedLinkedList.java
@@ -1,5 +1,21 @@
 public class InsertInSortedLinkedList {
     public ListNode insertInSortedLinkedList(ListNode head, int value) {
-
+        ListNode newNode = new ListNode(value);
+        if (head == null || head.value >= value) { // if null linked list or add at head directly
+            newNode.next = head;
+            return newNode;
+        }
+        ListNode prev = head;
+        while (prev.next != null && prev.next.value < value) { // if not the last node or the next's value is smaller than value
+            prev = prev.next;
+        }
+        newNode.next = prev.next;
+        prev.next = newNode;
+        return head;
     }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
