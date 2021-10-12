6e5ec0c93913deb4c78779fecd0a13470bc20366
shawn-yin128 <yxyoung128@gmail.com>
2021-10-12 01:48:19 -0400

    update

--- LinkedList/src/ReorderLinkedList.java
+++ LinkedList/src/ReorderLinkedList.java
@@ -1,2 +1,9 @@
+/**
+ * given a linked list N1 -> N2 -> N3 -> ... -> Nn
+ * reorder as N -> Nn -> N2 -> Nn-1 -> N3 -> Nn-2 -> ...
+ */
 public class ReorderLinkedList {
+    public ListNode split(ListNode head) {
+
+    }
 }
