bc5c9067456fd273c6979a4a20f6f67fc917c856
shawn-yin128 <yxyoung128@gmail.com>
2021-10-15 10:24:09 -0400

    update

--- LinkedList/src/PartitionLinkedList.java
+++ LinkedList/src/PartitionLinkedList.java
@@ -1,2 +1,34 @@
 public class PartitionLinkedList {
+    public ListNode partition(ListNode head, int target) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode smallHead = new ListNode(0);
+        ListNode largeHead = new ListNode(0);
+        ListNode small = smallHead;
+        ListNode large = largeHead;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            if (curr.value < target) {
+                small.next = curr;
+                curr.next = null;
+                small = small.next;
+                curr = next;
+            } else {
+                large.next = curr;
+                curr.next = null;
+                large = large.next;
+                curr = next;
+            }
+        }
+        small.next = largeHead.next;
+        largeHead.next = null;
+        return smallHead.next;
+    }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
