fa70ded87633c44f69e5e6bdd895bec379ef575a
shawn-yin128 <yxyoung128@gmail.com>
2021-10-15 16:14:44 -0400

    update

--- LinkedList/src/AddTwoNumbers.java
+++ LinkedList/src/AddTwoNumbers.java
@@ -1,2 +1,55 @@
 public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode left, ListNode right) {
+        if (left == null && right == null) {
+            return head;
+        }
+        ListNode rLeft = reverse(left);
+        ListNode rRight = reverse(right);
+        int carry = 0;
+        ListNode res = new ListNode(0);
+        while (rLeft != null && rRight != null) {
+            int temp = rLeft.value + rRight.value + carry;
+            int value = temp % 10;
+            carry = temp / 10;
+            res.next = new ListNode(value);
+            rLeft = rLeft.next;
+            rRight = rRight.next;
+            res = res.next;
+        }
+        while (rLeft != null) {
+            int temp = rLeft.value + carry;
+            int value = temp % 10;
+            carry = temp / 10;
+            res.next = new ListNode(value);
+            rLeft = rLeft.next;
+            res = res.next;
+        }
+        while (rRight != null) {
+            int temp = rRight.value + carry;
+            int value = temp % 10;
+            carry = temp / 10;
+            res.next = new ListNode(value);
+            rRight = rRight.next;
+            res = res.next;
+        }
+        if (carry != 0) {
+            res.next = new ListNode(carry);
+            res = res.next;
+        }
+        return res.next;
+    }
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse(head.next);
+        head.next.next = head.next;
+        head.next = null;
+        return newHead;
+    }
 }
+
+/**
+ * time complexity: O(n+m) + O(max(n, m)) = O(n+m)
+ * space complexity: O(n+m) + O(max(n ,m)) = O(n+m)
+ */
--- LinkedList/src/MergeSortLinkedList.java
+++ LinkedList/src/MergeSortLinkedList.java
@@ -1,2 +1,57 @@
 public class MergeSortLinkedList {
+    public ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode middle = findMiddle(head);
+        ListNode middleNext = middle.next;
+        middle.next = null;
+        ListNode left = mergeSort(head);
+        ListNode right = mergeSort(middleNext);
+        return merge(left, right);
+    }
+    public ListNode findMiddle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = haed;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode merge(ListNode left, ListNode right) {
+        if (left == null && right == null) {
+            return left;
+        }
+        ListNode newDummyHead = new ListNode(0);
+        ListNode curr = newDummyHead;
+        ListNode leftIndex = left;
+        ListNode rightIndex = right;
+        while (leftIndex != null && rightIndex != null) {
+            if (leftIndex.value < rightIndex.value) {
+                curr.next = leftIndex;
+                curr = curr.next;
+                leftIndex = leftIndex.next;
+            } else {
+                curr.next = rightIndex;
+                rightIndex = rightIndex.next;
+                curr = curr.next;
+            }
+        }
+        if (leftIndex != null) {
+            curr.next = leftIndex;
+        }
+        if (rightIndex != null) {
+            curr.next = rightIndex;
+        }
+        return newDummyHead.next;
+    }
 }
+
+/**
+ * time complexity: O(nlogn)
+ * space complexity: O(logn)
+ */
