87962769e749378162bd12a4c4252793ed0957ad
shawn-yin128 <yxyoung128@gmail.com>
2021-10-16 01:16:44 -0400

    update

--- LinkedList/src/RemoveLinkedListElement.java
+++ LinkedList/src/RemoveLinkedListElement.java
@@ -1,2 +1,28 @@
-public class RemoveLinkedListElement {
+public class RemoveLinkedListElement {1
+    public ListNode removeElement(ListNode head, int target) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        ListNode curr = dummyHead.next;
+        while (curr != null) {
+            if (curr.value != target) {
+                prev = curr;
+                curr = curr.next;
+            } else {
+                ListNode next = curr.next;
+                prev.next = next;
+                curr.next = null;
+                curr = next;
+            }
+        }
+        return dummyHead.next;
+    }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
