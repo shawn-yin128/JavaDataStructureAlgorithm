f2f0ca17c75bed7e6828aac65748ddde9d5e63dd
shawn-yin128 <yxyoung128@gmail.com>
2021-10-20 13:30:13 -0400

    update

--- BinaryTree/src/ThreeOrderTraverse.java
+++ BinaryTree/src/ThreeOrderTraverse.java
@@ -1,2 +1,41 @@
+/**
+ * class TreeNode {
+ *     public int value;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ * }
+ */
+
 public class ThreeOrderTraverse {
+    public void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.value);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.println(root.value);
+        inOrder(root.right);
+    }
+
+    public void postOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.value);
+    }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height)
+ */
