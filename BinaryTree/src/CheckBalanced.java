cf6a3f2899705c4853e3c0d90ff279d1ba316675
shawn-yin128 <yxyoung128@gmail.com>
2021-10-21 15:30:26 -0400

    update

---
+++ BinaryTree/src/CheckBalanced.java
@@ -0,0 +1,27 @@
+public class CheckBalanced {
+    public boolean isBalance(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (Math.abs(left - right) > 1) {
+            return false;
+        }
+        return isBalance(root.left) && isBalance(root.right);
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
+
+/**
+ * time complexity: O(nlogn)
+ * space complexity: O(height)
+ */
