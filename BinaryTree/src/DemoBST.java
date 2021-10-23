d68363c162471b5ad151cbb91614885a215d716a
shawn-yin128 <yxyoung128@gmail.com>
2021-10-23 05:05:22 -0400

    update

--- BinaryTree/src/DemoBST.java
+++ BinaryTree/src/DemoBST.java
@@ -23,6 +23,38 @@ public class DemoBST {
     }
 
     public TreeNode delete(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.key == key) {
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else if (root.right.left == null) {
+                root.right.left = root.left;
+                return root.right;
+            } else {
+                TreeNode newRoot = small(root.right);
+                newRoot.left = root.left;
+                newRoot.right = root.right;
+                return newRoot;
+            }
+        }
+        if (key < root.key) {
+            root.left = delete(root.left, key);
+        } else if (key > root.key) {
+            root.right = delete(root.right, key);
+        }
+        return root;
+    }
 
+    private TreeNode small(TreeNode root) {
+        while (root.left.left != null) {
+            root = root.left;
+        }
+        TreeNode smallest = root.left;
+        root.left = root.left.right;
+        return smallest;
     }
 }
