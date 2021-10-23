ad9491cb6f6b58b1839920bdbebbbce3d1fc5b6f
shawn-yin128 <yxyoung128@gmail.com>
2021-10-23 02:21:07 -0400

    update

---
+++ BinaryTree/src/CheckBinarySearchTree.java
@@ -0,0 +1,23 @@
+public class CheckBinarySearchTree {
+    public boolean isBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public boolean isBST(TreeNode root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.key <= min || root.key >= max) {
+            return false;
+        }
+        return isBST(root.left, min, root.key) && isBST(root.right, root.key, max);
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height)
+ */
