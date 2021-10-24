4fd64e91124e873c1bfe850a8cc9df4072cabf37
shawn-yin128 <yxyoung128@gmail.com>
2021-10-24 01:07:25 -0400

    update

--- BinaryTree/src/ThreeOrderTraverseIterative.java
+++ BinaryTree/src/ThreeOrderTraverseIterative.java
@@ -39,6 +39,32 @@ public class ThreeOrderTraverseIterative {
     } /* time complexity: O(n) space complexity: O(n+height) if only print it will be O(height) since max is the longest all right node */
 
     public List<Integer> postOrder(TreeNode root) {
-
-    }
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode prev = null;
+        stack.offerFirst(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peekFirst();
+            if (prev == null || cur == prev.left || cur == prev.right) {
+                if (cur.left != null) {
+                    stack.offerFirst(cur.left);
+                } else if (cur.right != null) {
+                    stack.offerFirst(cur.right);
+                } else {
+                    stack.pollFirst();
+                    result.add(cur.key);
+                }
+            } else if (prev == cur.right || (prev == cur.left && cur.right == null)) { // if no right node, add cur which is root in this case to result
+                stack.pollFirst();
+                result.add(cur.key);
+            } else {
+                stack.offerFirst(cur.right);
+            }
+            prev = cur;
+        }
+        return result;
+    } /* time complexity: O(n) space complexity: O(n+height) if only print it will be O(height) since max is the longest all right node */
 }
