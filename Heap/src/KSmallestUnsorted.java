2fe69bdbab5bd1543a04abfc7173527d31f0cf3b
shawn-yin128 <yxyoung128@gmail.com>
2021-10-25 21:49:19 -0400

    update

--- Heap/src/KSmallestUnsorted.java
+++ Heap/src/KSmallestUnsorted.java
@@ -1,2 +1,98 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class KSmallestUnsorted {
+    // min heap
+    public int[] kSmallestMinHeap(int[] array, int k) {
+        if (array == null || array.length == 0 || k == 0) {
+            return new int[0];
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(array.length);
+        for (int i = 0;  i < array.length; i++) {
+            minHeap.offer(array[i]);
+        }
+        int[] result = new int[k];
+        for (int j = 0; j < k; j++) {
+            result[j] = minHeap.poll();
+        }
+        return result;
+    }
+    // time complexity: O(nlogn + klogn) offer logn poll logn
+    // space complexity: O(n + k)
+
+    // max heap
+    public int[] kSmallestMaxHeap(int[] array, int k) {
+        if (array == null || array.length == 0 || k == 0) {
+            return new int[0];
+        }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1.equals(o2)) {
+                    return 0;
+                }
+                return o1 > o2 ? -1 : 1;
+            }
+        });
+        for (int i = 0; i < array.length; i++) {
+            if (i < k) {
+                maxHeap.offer(array[i]);
+            } else if (array[i] < maxHeap.peek()) {
+                maxHeap.poll();
+                maxHeap.offer(array[i]);
+            }
+        }
+        int[] result = new int[k];
+        for (int j = k - 1; j >= 0; j--) {
+            result[j] = maxHeap.poll();
+        }
+        return result;
+    }
+    // time complexity: O((n - k)logk + k)
+    // space complexity: O(k)
+
+    // quick select: use the idea of quick sort
+    public int[] kSmallestQuickSelect(int[] array, int k) {
+        if (array == null || array.length == 0 || k == 0) {
+            return new int[0];
+        }
+        quickSelect(array, 0, array.length - 1, k - 1);
+        int[] result = Arrays.copyOf(array, k);
+        Arrays.sort(result);
+        return result;
+    }
+    private void quickSelect(int[] array, int left, int right, int target) {
+        int mid = partition(array, left, right);
+        if (mid == target) {
+            return;
+        } else if (target < mid) {
+            quickSelect(array, left, mid - 1, target);
+        } else {
+            quickSelect(array, mid + 1, right, target);
+        }
+    }
+    private int partition(int[] array, int left, int right) {
+        int pivot = array[right];
+        int start = left;
+        int end = right - 1;
+        while (start <= end) {
+            if (array[start] < pivot) {
+                start++;
+            } else if (array[end] >= pivot) {
+                end--;
+            } else {
+                swap(array, start++, end--);
+            }
+        }
+        swap(array, start, right);
+        return start;
+    }
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+    // time complexity: O(n) on average
+    // space complexity: O(logn)
 }
