2b5160d56476fe073957e6cf776476caa333d3df
shawn-yin128 <yxyoung128@gmail.com>
2021-10-26 21:17:09 -0400

    update

---
+++ GraphSearch/BestFirstSearch/src/TreeNode.java
@@ -0,0 +1,5 @@
+public class TreeNode {
+    public int key;
+    public TreeNode left;
+    public TreeNode right;
+}
--- GraphSearch/BreadthFirstSearch/src/LevelTraverseBinaryTree.java
+++ GraphSearch/BreadthFirstSearch/src/LevelTraverseBinaryTree.java
@@ -1,2 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class LevelTraverseBinaryTree {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> curLayer = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                curLayer.add(cur.key);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            result.add(curLayer);
+        }
+        return result;
+    }
 }
