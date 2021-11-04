f0224a716444c6dd525fa57f886b0e0da5b74a91
shawn-yin128 <yxyoung128@gmail.com>
2021-11-04 03:50:17 -0400

    update

--- String/src/Remove/RemoveCertainChar.java
+++ String/src/Remove/RemoveCertainChar.java
@@ -1,10 +1,36 @@
 package Remove;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Remove given characters in input string, the relative order of other characters should be remained. Return the new string after deletion.
  */
 public class RemoveCertainChar {
-    public String remove(String input, String set) {
-
+    public String remove(String input, String target) {
+        char[] array = input.toCharArray();
+        Set<Character> set = buildSet(target);
+        int slow = 0;
+        int fast = 0;
+        while (fast < array.length) {
+            if(set.contains(array[fast])) {
+                fast++;
+            } else {
+                array[slow++] = array[fast++];
+            }
+        }
+        return new String(array, 0, slow);
+    }
+    private Set<Character> buildSet(String string) {
+        Set<Character> result = new HashSet<>();
+        for (int i = 0; i < string.length(); i++) {
+            result.add(string.charAt(i));
+        }
+        return result;
     }
 }
+
+/**
+ * time complexity: O(m + n) m and n are number of element in input and target
+ * space complexity: O(m + n)
+ */
