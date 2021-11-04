0ed73e49a9ad5e9f94d9a29f1ba2eb572860d41c
shawn-yin128 <yxyoung128@gmail.com>
2021-11-04 04:04:38 -0400

    update remove space in string

--- String/src/DeDuplicate/RemoveAdjacentRepeat.java
+++ String/src/DeDuplicate/RemoveAdjacentRepeat.java
@@ -1,4 +1,25 @@
 package DeDuplicate;
 
+/**
+ * Remove adjacent, repeated characters in a given string, leaving only one character for each group of such characters.
+ */
 public class RemoveAdjacentRepeat {
+    public String deDuplicate(String input) {
+        char[] array = input.toCharArray();
+        int slow = 0;
+        int fast = 0;
+        while (fast < array.length) {
+            if (fast == 0 || array[fast] != array[slow - 1]) {
+                array[slow++] = array[fast++];
+            } else {
+                fast++;
+            }
+        }
+        return new String(array, 0, slow);
+    }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(n)
+ */
