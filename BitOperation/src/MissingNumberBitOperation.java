bd1aad8bf038f7d183633f0e606413826fc04746
shawn-yin128 <yxyoung128@gmail.com>
2021-11-04 02:32:48 -0400

    update

--- BitOperation/src/MissingNumberBitOperation.java
+++ BitOperation/src/MissingNumberBitOperation.java
@@ -3,6 +3,18 @@
  */
 public class MissingNumberBitOperation {
     public int missNumber(int[] array) {
-
+        int n = array.length + 1;
+        int xor = 0;
+        for (int i = 1; i <= n; i++) {
+            xor ^= i;
+        }
+        for (int num : array) {
+            xor ^= num;
+        }
+        return xor;
     }
 }
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
