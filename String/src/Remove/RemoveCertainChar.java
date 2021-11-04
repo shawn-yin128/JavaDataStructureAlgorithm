912b7bfcf3c4f703f44e9d712f003acc191571ce
shawn-yin128 <yxyoung128@gmail.com>
2021-11-04 03:42:23 -0400

    update

--- String/src/Remove/RemoveCertainChar.java
+++ String/src/Remove/RemoveCertainChar.java
@@ -1,4 +1,10 @@
 package Remove;
 
+/**
+ * Remove given characters in input string, the relative order of other characters should be remained. Return the new string after deletion.
+ */
 public class RemoveCertainChar {
+    public String remove(String input, String set) {
+
+    }
 }
