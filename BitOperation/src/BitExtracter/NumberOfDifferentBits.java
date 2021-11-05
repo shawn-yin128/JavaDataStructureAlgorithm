857b1847c173be8ec8b76dfbfc840c51e8e6772c
shawn-yin128 <yxyoung128@gmail.com>
2021-11-05 02:09:44 -0400

    update

---
+++ BitOperation/src/BitExtracter/NumberOfDifferentBits.java
@@ -0,0 +1,4 @@
+package BitExtracter;
+
+public class NumberOfDifferentBits {
+}
--- HashMap/src/DemoHashMap/DemoHashMap.java
+++ HashMap/src/DemoHashMap/DemoHashMap.java
@@ -1,3 +1,23 @@
 package DemoHashMap;
 public class DemoHashMap {
+    public static class Entry<K, V> {
+        public K key;
+        public V value;
+        Entry<K, V> next;
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
 }
