fd04bdc835ad2eda5f0bf13f1e9ba5250e8df618
shawn-yin128 <yxyoung128@gmail.com>
2021-11-06 18:59:10 -0400

    update reverse string

--- String/src/Reverse/ReverseString.java
+++ String/src/Reverse/ReverseString.java
@@ -1,4 +1,44 @@
 package Reverse;
 
+/**
+ * Reverse a given string.
+ */
 public class ReverseString {
+    // iterative
+    public String reverseStrIterative(String input) {
+        if (input == null || input.length() <= 1) {
+            return input;
+        }
+        char[] array = input.toCharArray();
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            swap(array, left++, right--);
+        }
+        return new String(array);
+    } // time complexity: O(n), space complexity: O(n)
+
+    // recursive
+    public String reverseStrRecursive(String input) {
+        if (input == null || input.length() <= 1) {
+            return input;
+        }
+        char[] array = input.toCharArray();
+        helper(array, 0, array.length - 1);
+        return new String(array);
+    }
+    private void helper(char[] array, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        swap(array, left, right);
+        helper(array, left + 1, right - 1);
+    } // time complexity: O(n), space complexity: O(n)
+
+    // common method
+    private void swap(char[] array, int left, int right) {
+        char temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
 }
