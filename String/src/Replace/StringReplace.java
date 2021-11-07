d8d0b1f8410ac9e6908c068040082798fcaca094
shawn-yin128 <yxyoung128@gmail.com>
2021-11-06 22:28:01 -0400

    update

---
+++ String/src/Replace/StringReplace.java
@@ -0,0 +1,24 @@
+package Replace;
+
+import java.util.List;
+
+/**
+ * Given an original string input, and two strings S and T, from left to right replace all occurrences of S in input with T.
+ */
+public class StringReplace {
+    public String replace(String input, String source, String target) {
+
+    }
+
+    public String replaceShort(char[] array, String source, String target) {
+
+    }
+
+    public String replaceLonger(char[] array, String source, String target) {
+
+    }
+
+    private List<Integer> getAllMatch(char[] array, String source) {
+        
+    }
+}
