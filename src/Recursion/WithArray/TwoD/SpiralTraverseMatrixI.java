1af553b07079a8035d9b915110fb6cad2c3cb4c8
shawn-yin128 <yxyoung128@gmail.com>
2021-11-08 01:36:20 -0500

    update spiral traverse matrix

---
+++ src/Recursion/WithArray/TwoD/SpiralTraverseMatrixI.java
@@ -0,0 +1,38 @@
+package WithArray.TwoD;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Traverse an N * N 2D array in spiral order clock-wise starting from the top left corner. Return the list of traversal sequence.
+ */
+public class SpiralTraverseMatrixI {
+    public List<Integer> spiral(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        helper(matrix, 0, matrix.length, result);
+        return result;
+    }
+
+    private void helper(int[][] matrix, int offset, int size, List<Integer> result) {
+        if (size == 0) {
+            return;
+        }
+        if (size == 1) {
+            result.add(matrix[offset][offset]);
+            return;
+        }
+        for (int i = 0; i < size - 1; i++) {
+            result.add(matrix[offset][offset + i]);
+        }
+        for (int i = 0; i < size - 1; i++) {
+            result.add(matrix[offset + i][offset + size - 1]);
+        }
+        for (int i = size - 1; i > 0; i--) {
+            result.add(matrix[offset + size - 1][offset + i]);
+        }
+        for (int i = size - 1; i > 0; i--) {
+            result.add(matrix[offset + i][offset]);
+        }
+        helper(matrix, offset + 1, size - 2, result);
+    }
+}
