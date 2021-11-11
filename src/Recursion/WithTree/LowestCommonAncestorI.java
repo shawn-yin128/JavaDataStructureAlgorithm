7090b6cc5b63ed7ea5fd7c81e11fe7d3b445119f
shawn-yin128 <yxyoung128@gmail.com>
2021-11-11 11:33:12 -0500

    update lca

---
+++ src/Recursion/WithTree/LowestCommonAncestorI.java
@@ -0,0 +1,37 @@
+package WithTree;
+
+/**
+ * Given two nodes in a binary tree, find their lowest common ancestor.
+ * There is no parent pointer for the nodes in the binary tree
+ * The given two nodes are guaranteed to be in the binary tree
+ */
+public class LowestCommonAncestorI {
+    public static class TreeNode {
+        public int key;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value) {
+            key = value;
+        }
+    }
+
+    public TreeNode LCA(TreeNode root, TreeNode one, TreeNode two) {
+        if (root == null) {
+            return null;
+        }
+        if (root == one || root == two) {
+            return root;
+        }
+        TreeNode left = LCA(root.left, one, two);
+        TreeNode right = LCA(root.right, one, two);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height)
+ */
