023990e8df4d5f29391d25ee7b41969217be1500
shawn-yin128 <yxyoung128@gmail.com>
2021-11-11 16:54:16 -0500

    update array hopper

---
+++ src/DynamicProgramming/OneD/ArrayHopper.java
@@ -0,0 +1,32 @@
+package OneD;
+
+/**
+ * Given an array A of non-negative integers, you are initially positioned at index 0 of the array. A[i] means the maximum jump distance from that position (you can only jump towards the end of the array). Determine if you are able to reach the last index.
+ */
+public class ArrayHopper {
+    public boolean jump(int[] array) {
+        if (array.length == 1) {
+            return true;
+        }
+        boolean[] canJump = new boolean[array.length];
+        canJump[array.length - 1] = true;
+        for (int i = array.length - 2; i >= 0; i--) {
+            if (i + array[i] >= array.length - 1) {
+                canJump[i] = true;
+            } else {
+                for (int j = array[i]; j >= 1; j--) {
+                    if (canJump[i + j]) {
+                        canJump[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return canJump[0];
+    }
+}
+
+/**
+ * time complexity: O(n^2)
+ * space complexity: O(n)
+ */
