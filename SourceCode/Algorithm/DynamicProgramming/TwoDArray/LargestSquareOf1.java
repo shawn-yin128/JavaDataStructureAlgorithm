f1d90f0a6b4fcee51b71ba308b366e4bfb21f553
shawn-yin128 <yxyoung128@gmail.com>
2021-11-13 02:53:28 -0500

    update

--- SourceCode/Algorithm/DynamicProgramming/TwoDArray/LargestSquareOf1.java
+++ SourceCode/Algorithm/DynamicProgramming/TwoDArray/LargestSquareOf1.java
@@ -4,5 +4,27 @@ package Algorithm.DynamicProgramming.TwoDArray;
  * Determine the largest square of 1s in a binary matrix (a binary matrix only contains 0 and 1), return the length of the largest square.
  */
 public class LargestSquareOf1 {
-    
+    public int largest(int[][] matrix) {
+        if (matrix.length == 0) {
+            return 0;
+        }
+        int result = 0;
+        int[][] largest = new int[matrix.length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i == 0 || j == 0) {
+                    largest[i][j] = matrix[i][j] == 1 ? 1 : 0;
+                } else if (matrix[i][j] == 1) {
+                    largest[i][j] = Math.min(largest[i - 1][j - 1] + 1, Math.min(largest[i][j - 1] + 1, largest[i - 1][j] + 1));
+                }
+                result = Math.max(result, largest[i][j]);
+            }
+        }
+        return result;
+    }
 }
+
+/**
+ * time complexity: O(n^2)
+ * space complexity: O(n^2)
+ */
---
+++ SourceCode/DataStructure/Tree/CommonBinaryTree/AdvancedTreeRecursion/NumberOfLeftSubtree.java
@@ -0,0 +1,31 @@
+package DataStructure.Tree.CommonBinaryTree.AdvancedTreeRecursion;
+
+/**
+ * Given a binary tree, count the number of nodes in each node’s left subtree, and store it in the numNodesLeft field.
+ */
+public class NumberOfLeftSubtree {
+    public static class TreeNode {
+        public int key;
+        public TreeNode left;
+        public TreeNode right;
+        public int numNodesLeft;
+        public TreeNode(int key) {
+            this.key = key;
+        }
+    }
+
+    public int numberLeft(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = numberLeft(root.left);
+        int right = numberLeft(root.right);
+        root.numNodesLeft = left;
+        return (left + right + 1);
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height)
+ */
