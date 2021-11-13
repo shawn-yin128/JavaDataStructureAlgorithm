1ead3c8df88d07425321338634a3736da31bb6cf
shawn-yin128 <yxyoung128@gmail.com>
2021-11-13 01:58:13 -0500

    update number of different bits

--- SourceCode/Algorithm/BitOperation/BitExtracter/NumberOfDifferentBits.java
+++ SourceCode/Algorithm/BitOperation/BitExtracter/NumberOfDifferentBits.java
@@ -1,4 +1,21 @@
 package Algorithm.BitOperation.BitExtracter;
 
+/**
+ * Determine the number of bits that are different for two given integers.
+ */
 public class NumberOfDifferentBits {
+    public int diffBits(int a, int b) {
+        a ^= b;
+        int count = 0;
+        while (a != 0) {
+            count += (a & 1);
+            a >>>= 1;
+        }
+        return count;
+    }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
