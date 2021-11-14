13d717034588d5513e81fcaf69fd4c0e59be286a
shawn-yin128 <yxyoung128@gmail.com>
2021-11-14 15:53:12 -0500

    update max path sum from one straight path

---
+++ SourceCode/Algorithm/Recursion/WithTree/MaxPathSumIII.java
@@ -0,0 +1,38 @@
+package Algorithm.Recursion.WithTree;
+
+/**
+ * Given a binary tree in which each node contains an integer number. Find the maximum possible subpath sum
+ * both the starting and ending node of the subpath should be on the same path from root to one of the leaf nodes, and the subpath is allowed to contain only one node.
+ */
+public class MaxPathSumIII {
+    public static class TreeNode {
+        public int key;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value) {
+            key = value;
+        }
+    }
+
+    public int maxPathSum(TreeNode root) {
+        int[] result = new int[] {Integer.MIN_VALUE};
+        helper(root, result);
+        return result[0];
+    }
+
+    private int helper(TreeNode root, int[] result) {
+        if (root == null) {
+            return 0;
+        }
+        int left = helper(root.left, result);
+        int right = helper(root.right, result);
+        int cur = Math.max(Math.max(left, right), 0) + root.key;
+        result[0] = Math.max(result[0], cur);
+        return cur;
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height)
+ */
