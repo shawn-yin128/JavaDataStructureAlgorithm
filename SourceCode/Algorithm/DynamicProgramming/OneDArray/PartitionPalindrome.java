4e2b5e407e4d7f8c4c7a1f8c5e26a31e12f12468
shawn-yin128 <yxyoung128@gmail.com>
2021-11-14 17:14:53 -0500

    update string partition palindrome

---
+++ SourceCode/Algorithm/DynamicProgramming/OneDArray/PartitionPalindrome.java
@@ -0,0 +1,39 @@
+package Algorithm.DynamicProgramming.OneDArray;
+
+/**
+ * Given a string, a partitioning of the string is a palindrome partitioning if every partition is a palindrome.
+ * minimum 3 cuts are needed for “ababbbabbababa”. The three cuts are “a | babbbab | b | ababa”.
+ */
+public class PartitionPalindrome {
+    public int minCutPalindrome(String input) {
+        int[] cut = new int[input.length() + 1];
+        cut[0] = 0;
+        cut[1] = 0;
+        for (int i = 2; i < cut.length; i++) {
+            cut[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < i; j++) {
+                if (isPalindrome(input.substring(j, i))) {
+                    cut[i] = 0;
+                    break;
+                } else {
+                    if (isPalindrome(input.substring(j, i))) {
+                        cut[i] = Math.min(cut[i], cut[j] + 1);
+                    }
+                }
+            }
+        }
+        return cut[cut.length - 1];
+    }
+
+    private boolean isPalindrome(String input) {
+        char[] array = input.toCharArray();
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            if (array[left] != array[right]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
