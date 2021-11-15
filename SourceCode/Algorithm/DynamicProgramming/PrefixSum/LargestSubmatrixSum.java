51f1c0dd2957390e32441a28970ce035a42b4a2d
shawn-yin128 <yxyoung128@gmail.com>
2021-11-15 03:25:35 -0500

    update largest submatrix sum

--- SourceCode/Algorithm/DynamicProgramming/PrefixSum/LargestSubmatrixSum.java
+++ SourceCode/Algorithm/DynamicProgramming/PrefixSum/LargestSubmatrixSum.java
@@ -1,4 +1,8 @@
 package Algorithm.DynamicProgramming.PrefixSum;
 
+/**
+ * Given a matrix that contains integers, find the submatrix with the largest sum.
+ * Return the sum of the submatrix.
+ */
 public class LargestSubmatrixSum {
 }
