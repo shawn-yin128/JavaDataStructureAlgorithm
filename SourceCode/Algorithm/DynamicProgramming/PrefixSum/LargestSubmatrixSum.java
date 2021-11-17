c437d713869e6f59b7462b0ddd0d4c8a9db3668b
shawn-yin128 <yxyoung128@gmail.com>
2021-11-16 19:33:57 -0500

    update largest submatrix sum

--- SourceCode/Algorithm/DynamicProgramming/PrefixSum/LargestSubmatrixSum.java
+++ SourceCode/Algorithm/DynamicProgramming/PrefixSum/LargestSubmatrixSum.java
@@ -5,4 +5,39 @@ package Algorithm.DynamicProgramming.PrefixSum;
  * Return the sum of the submatrix.
  */
 public class LargestSubmatrixSum {
+    public int largestSubmatrix(int[][] matrix) {
+        if (matrix == null) {
+            return 0;
+        }
+        int n = matrix.length;
+        if (n == 0) {
+            return 0;
+        }
+        int m = matrix[0].length;
+        if (m == 0) {
+            return 0;
+        }
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int[] cur = new int[m];
+            for (int j = i; j < n; j++) {
+                for (int k = 0; k < m; k++) {
+                    cur[k] += matrix[j][k];
+                }
+                int curMax = cur[0];
+                int prev = cur[0];
+                for (int l = 1; l < m; l++) {
+                    prev = Math.max(prev + cur[l], cur[l]);
+                    curMax = Math.max(curMax, prev);
+                }
+                result = Math.max(result, curMax);
+            }
+        }
+        return result;
+    }
 }
+
+/**
+ * time complexity: O(N * N * M)
+ * space complexity: O(N * M)
+ */
