aa043417405d02e7e7f719df4e6fe78bb1144a63
shawn-yin128 <yxyoung128@gmail.com>
2021-11-22 00:05:14 -0500

    update array deduplicate with

--- SourceCode/DataStructure/Array/TwoPointers/ArrayDeduplicateIII.java
+++ SourceCode/DataStructure/Array/TwoPointers/ArrayDeduplicateIII.java
@@ -1,5 +1,7 @@
 package DataStructure.Array.TwoPointers;
 
+import java.util.Arrays;
+
 /**
  * Given a sorted integer array, remove duplicate elements.
  * For each group of elements with the same value do not keep any of them.
@@ -7,4 +9,27 @@ package DataStructure.Array.TwoPointers;
  * Return the array after deduplication.
  */
 public class ArrayDeduplicateIII {
+    public int[] sortedArrayDeduplicate(int[] array) {
+        if (array == null || array.length == 0) {
+            return array;
+        }
+        int slow = 0;
+        int fast = 0;
+        while (fast < array.length) {
+            if (slow == 0 || array[fast] != array[slow - 1]) {
+                array[slow++] = array[fast++];
+            } else {
+                while (fast < array.length && array[fast] == array[slow - 1]) {
+                    fast++;
+                }
+                slow--;
+            }
+        }
+        return Arrays.copyOfRange(array, 0, slow);
+    }
 }
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(1)
+ */
