0379103696fcbc1e0f0729c877297d60d48edebd
shawn-yin128 <yxyoung128@gmail.com>
2021-11-26 18:20:41 -0500

    update LCA of k nodes

---
+++ SourceCode/Algorithm/Recursion/WithTree/LowestCommonAncestorIV.java
@@ -0,0 +1,47 @@
+package Algorithm.Recursion.WithTree;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Given K nodes in a binary tree, find their lowest common ancestor
+ */
+public class LowestCommonAncestorIV {
+    public static class TreeNode {
+        public int key;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value) {
+            key = value;
+        }
+    }
+
+    public TreeNode LCA(TreeNode root, List<TreeNode> list) {
+        if (root == null || list == null) {
+            return null;
+        }
+        Set<TreeNode> set = new HashSet<>(list);
+        return helper(root, set);
+    }
+
+    private TreeNode helper(TreeNode root, Set<TreeNode> set) {
+        if (root == null) {
+            return null;
+        }
+        if (set.contains(root)) {
+            return root;
+        }
+        TreeNode left = helper(root.left, set);
+        TreeNode right = helper(root.right, set);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height + k) k is the size of list because we new a set of that list
+ */
