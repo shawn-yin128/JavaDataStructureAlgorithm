fc999666112af509da104b4bfd0be2f8a73f2292
shawn-yin128 <yxyoung128@gmail.com>
2021-11-25 22:26:54 -0500

    update find LCA in binary tree that each node has parent pointer

---
+++ SourceCode/Algorithm/Recursion/WithTree/LowestCommonAncestorII.java
@@ -0,0 +1,51 @@
+package Algorithm.Recursion.WithTree;
+
+/**
+ * Given two nodes in a binary tree (with parent pointer available), find their lowest common ancestor.
+ */
+public class LowestCommonAncestorII {
+    public static class TreeNodeP {
+        public int key;
+        public TreeNodeP left;
+        public TreeNodeP right;
+        public TreeNodeP parent;
+        public TreeNodeP(int value, TreeNodeP parent) {
+            key = value;
+            this.parent = parent;
+        }
+    }
+
+    public TreeNodeP LCA(TreeNodeP one, TreeNodeP two) {
+        int oneLength = getHeight(one);
+        int twoLength = getHeight(two);
+        if (oneLength > twoLength) {
+            return LCAHelper(two, one, oneLength - twoLength);
+        } else {
+            return LCAHelper(one, two, twoLength - oneLength);
+        }
+    }
+
+    private TreeNodeP LCAHelper(TreeNodeP shorter, TreeNodeP longer, int diff) {
+        while (diff > 0) {
+            shorter = shorter.parent;
+            diff--;
+        }
+        while (shorter != null && longer != null) {
+            if (shorter == longer) {
+                return longer;
+            }
+            shorter = shorter.parent;
+            longer = longer.parent;
+        }
+        return null;
+    }
+
+    private int getHeight(TreeNodeP root) {
+        int length = 0;
+        while (root != null) {
+            root = root.parent;
+            length++;
+        }
+        return length;
+    }
+}
