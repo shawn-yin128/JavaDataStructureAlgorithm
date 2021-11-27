b7985b3d02d3b1fa424bf3d22d81bf5345d36b51
shawn-yin128 <yxyoung128@gmail.com>
2021-11-26 19:45:43 -0500

    update LCA of k nary

---
+++ SourceCode/Algorithm/Recursion/WithTree/LowestCommonAncestorV.java
@@ -0,0 +1,54 @@
+package Algorithm.Recursion.WithTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given two nodes in a K-nary tree, find their lowest common ancestor.
+ */
+public class LowestCommonAncestorV {
+    public static class KnaryTreeNode {
+        int key;
+        List<KnaryTreeNode> children;
+
+        public KnaryTreeNode(int key) {
+            this.key = key;
+            this.children = new ArrayList<>();
+        }
+    }
+
+    public KnaryTreeNode LCA(KnaryTreeNode root, KnaryTreeNode one, KnaryTreeNode two) {
+        if (root == null) {
+            return null;
+        }
+        return helper(root, one, two);
+    }
+
+    private KnaryTreeNode helper(KnaryTreeNode root, KnaryTreeNode one, KnaryTreeNode two) {
+        if (root == null) {
+            return null;
+        }
+        if (root == one || root == two) {
+            return root;
+        }
+        KnaryTreeNode result = null;
+        for (KnaryTreeNode node : root.children) {
+            KnaryTreeNode lca = helper(node, one, two);
+            if (lca == null) {
+                continue;
+            } else {
+                if (result == null) {
+                    result = lca;
+                } else {
+                    return root;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(height)
+ */
