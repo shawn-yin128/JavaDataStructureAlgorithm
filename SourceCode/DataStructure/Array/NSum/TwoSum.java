7a423ea832d20ebc154e30b954d9a66f6ebddd95
shawn-yin128 <yxyoung128@gmail.com>
2021-11-28 00:33:50 -0500

    update two sum

--- SourceCode/DataStructure/Array/NSum/TwoSum.java
+++ SourceCode/DataStructure/Array/NSum/TwoSum.java
@@ -9,7 +9,6 @@ import java.util.Set;
  * Determine if there exist two elements in a given array, the sum of which is the given target number.
  */
 public class TwoSum {
-    // simple version that return true if has such a pair and false if not
     public boolean twoSumI(int[] array, int target) {
         if (array == null || array.length == 0) {
             return false;
@@ -24,27 +23,6 @@ public class TwoSum {
             }
         }
         return false;
-    }
-
-    // return all pairs
-    public List<List<Integer>> twoSumII(int[] array, int target) {
-        if (array == null || array.length == 0) {
-            return null;
-        }
-        List<List<Integer>> result = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
-        for (int num : array) {
-            int res = target - num;
-            if (set.contains(res)) {
-                List<Integer> cur = new ArrayList<>();
-                cur.add(num);
-                cur.add(res);
-                result.add(cur);
-            } else {
-                set.add(num);
-            }
-        }
-        return result;
     }
 }
 
