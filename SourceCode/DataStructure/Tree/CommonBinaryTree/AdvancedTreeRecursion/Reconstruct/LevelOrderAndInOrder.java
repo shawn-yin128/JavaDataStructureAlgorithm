34d90bc53cdb89ed7f2f94a8a0c9f303b60d015d
shawn-yin128 <yxyoung128@gmail.com>
2021-12-09 21:23:03 -0500

    update reconstruct using level order and in order

---
+++ SourceCode/DataStructure/Tree/CommonBinaryTree/AdvancedTreeRecursion/Reconstruct/LevelOrderAndInOrder.java
@@ -0,0 +1,57 @@
+package DataStructure.Tree.CommonBinaryTree.AdvancedTreeRecursion.Reconstruct;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given the levelorder and inorder traversal sequence of a binary tree, reconstruct the original tree.
+ */
+public class LevelOrderAndInOrder {
+    public class TreeNode {
+        public int key;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int value) {
+            key = value;
+        }
+    }
+
+    public TreeNode reconstruct(int[] inOrder, int[] levelOrder) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+        for (int i = 0; i < inOrder.length; i++) {
+            inMap.put(inOrder[i], i);
+        }
+        List<Integer> level = new ArrayList<>();
+        for (int num : levelOrder) {
+            level.add(num);
+        }
+        return helper(level, inMap);
+    }
+
+    private TreeNode helper(List<Integer> level, Map<Integer, Integer> inMap) {
+        if (level.isEmpty()) {
+            return null;
+        }
+        TreeNode root = new TreeNode(level.remove(0));
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        for (int num : level) {
+            if (inMap.get(num) < inMap.get(root.key)) {
+                left.add(num);
+            } else {
+                right.add(num);
+            }
+        }
+        root.left = helper(left, inMap);
+        root.right = helper(right, inMap);
+        return root;
+    }
+}
+
+/**
+ * time complexity: O(n)
+ * space complexity: O(n)
+ */
